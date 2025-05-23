8baac9fe11f2176e115c356d54eed34ec768a8ef
DaviRodriFv <davir2493@gmail.com>
2025-05-22 21:55:52 -0300

    Metodo contar Pre-Ordem nao recursivo

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import org.w3c.dom.Node;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
@@ -69,6 +70,28 @@ public class Main {
                     }
                     if (atual.right != null) {
                         fila.add(atual.right);
+                    }
+                }
+            }
+
+
+            //METODO PRE-ORDEM (NAO RECURSIVO)
+            void preOrdemNaoRecursivo(Node root) {
+                if (root == null)
+                    return;
+
+                Stack<Node> pilha = new Stack<>();
+                pilha.push(root);
+
+                while (!pilha.isEmpty()) {
+                    Node atual = pilha.pop();
+                    System.out.print(atual.valor + " ");
+
+                    if (atual.right != null) {
+                        pilha.push(atual.right);
+                    }
+                    if (atual.left != null) {
+                        pilha.push(atual.left);
                     }
                 }
             }
