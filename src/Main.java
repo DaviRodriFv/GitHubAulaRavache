53cb0311d23b0097b93d805842c3bf7cb5f7ae57
DaviRodriFv <davir2493@gmail.com>
2025-05-22 21:24:58 -0300

    Arvores com nós

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import org.w3c.dom.Node;
+
+public class Main {
+    public static void main(String[] args) {
+
+        class Node{
+            int num;
+            Node left;
+            Node right;
+
+            public Node(int num){
+                this.num = num;
+            }
+
+        }
+        class ArvoreBinaria{
+            Node root;
+        }
+
+    }
+}
