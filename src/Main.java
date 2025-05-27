521463c1c29015e73f4b6a062073f06f8beec19c
DaviRodriFv <davir2493@gmail.com>
2025-05-27 19:18:55 -0300

    Metodo Pos Ordem Nao recursivo

--- src/Main.java
+++ src/Main.java
@@ -125,6 +125,31 @@ public class Main {
                 }
             }
 
+            //METODO POS-ORDEM (NAO RECURSIVO)
+            void posOrdemNaoRecursivo(Node root) {
+                if (root == null)
+                    return;
+
+                Stack<Node> stack1 = new Stack<>();
+                Stack<Node> stack2 = new Stack<>();
+
+                stack1.push(root);
+
+                while (!stack1.isEmpty()) {
+                    Node node = stack1.pop();
+                    stack2.push(node);
+
+                    if (node.left != null)
+                        stack1.push(node.left);
+                    if (node.right != null)
+                        stack1.push(node.right);
+                }
+
+                while (!stack2.isEmpty()) {
+                    System.out.print(stack2.pop().valor + " ");
+                }
+            }
+
 
         }
 
